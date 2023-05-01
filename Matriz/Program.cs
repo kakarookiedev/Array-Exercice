cbd888a02275f46f26c139dd8002aca6de4c5b6e
Kauan <Kauan@DESKTOP-UEKMN1L>
2023-05-01 16:31:01 -0300

    Add project files.

---
+++ Matriz/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("INPUT ARRAY DIMENSIONS: ");
+            int n = int.Parse(Console.ReadLine());
+            int negativeNum = 0;
+            int[,] matriz = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] value = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = int.Parse(value[j]);
+                    if (matriz[i, j] < 0)
+                    {
+                        negativeNum++;
+                    }
+                }
+            }
+            Console.WriteLine("\nMAIN DIAGONAL: ");
+            for (int i = 0; i < n; i++)
+            {
+                int j = i;
+                Console.Write(matriz[i, j] + " ");
+
+            }
+            Console.WriteLine($"\nNEGATIVE NUMS COUNT = {negativeNum}");
+        }
+    }
+}
